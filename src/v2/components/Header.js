5e4e3a20b5ad901355614406f9340d34bea2933b
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-06-15 14:20:49 -0600

    Add tech stack icons next to text

--- src/v2/components/Header.js
+++ src/v2/components/Header.js
@@ -3,6 +3,67 @@ import { Github, Linkedin, Mail } from 'lucide-react';
 import { skills } from '../data/skills';
 import profileImage from './img/jaykim.png';
 
+import { ReactComponent as ReactIcon } from '../data/skills/react.svg';
+import { ReactComponent as JavascriptIcon } from '../data/skills/javascript.svg';
+import { ReactComponent as TypescriptIcon } from '../data/skills/typescript.svg';
+import { ReactComponent as NextjsIcon } from '../data/skills/nextjs.svg';
+import { ReactComponent as HtmlIcon } from '../data/skills/html.svg';
+import { ReactComponent as CssIcon } from '../data/skills/css.svg';
+import { ReactComponent as BootstrapIcon } from '../data/skills/bootstrap.svg';
+import { ReactComponent as ReactnativeIcon } from '../data/skills/reactnative.svg';
+import { ReactComponent as ExpressjsIcon } from '../data/skills/expressjs.svg';
+import { ReactComponent as DjangoIcon } from '../data/skills/django.svg';
+import { ReactComponent as PythonIcon } from '../data/skills/python.svg';
+import { ReactComponent as JavaIcon } from '../data/skills/java.svg';
+import { ReactComponent as FastapiIcon } from '../data/skills/fastapi.svg';
+import { ReactComponent as MysqlIcon } from '../data/skills/mysql.svg';
+import { ReactComponent as MariadbIcon } from '../data/skills/mariadb.svg';
+import { ReactComponent as MicrosoftsqlserverIcon } from '../data/skills/microsoftsqlserver.svg';
+import { ReactComponent as AwsIcon } from '../data/skills/aws.svg';
+import { ReactComponent as FirebaseIcon } from '../data/skills/firebase.svg';
+import { ReactComponent as VscodeIcon } from '../data/skills/vscode.svg';
+import { ReactComponent as GitIcon } from '../data/skills/git.svg';
+import { ReactComponent as NpmIcon } from '../data/skills/npm.svg';
+import { ReactComponent as YarnIcon } from '../data/skills/yarn.svg';
+import { ReactComponent as PostmanIcon } from '../data/skills/postman.svg';
+import { ReactComponent as FigmaIcon } from '../data/skills/figma.svg';
+import { ReactComponent as SlackIcon } from '../data/skills/slack.svg';
+import { ReactComponent as NotionIcon } from '../data/skills/notion.svg';
+import { ReactComponent as NodejsIcon } from '../data/skills/nodejs.svg';
+
+import bootstrapSvg from '../data/skills/bootstrap.svg';
+import mariadbSvg from '../data/skills/mariadb.svg';
+
+const iconMap = {
+  'React': ReactIcon,
+  'JavaScript': JavascriptIcon,
+  'TypeScript': TypescriptIcon,
+  'Next.js': NextjsIcon,
+  'HTML': HtmlIcon,
+  'CSS': CssIcon,
+  'Bootstrap': BootstrapIcon,
+  'React Native': ReactnativeIcon,
+  'Node.js': NodejsIcon,
+  'Express.js': ExpressjsIcon,
+  'Django': DjangoIcon,
+  'Python': PythonIcon,
+  'Java': JavaIcon,
+  'Fast API': FastapiIcon,
+  'MySQL': MysqlIcon,
+  'SQL': MicrosoftsqlserverIcon,
+  'MariaDB': MariadbIcon,
+  'AWS': AwsIcon,
+  'Firebase': FirebaseIcon,
+  'VSCode': VscodeIcon,
+  'Git': GitIcon,
+  'npm': NpmIcon,
+  'yarn': YarnIcon,
+  'Postman': PostmanIcon,
+  'Figma': FigmaIcon,
+  'Slack': SlackIcon,
+  'Notion': NotionIcon,
+};
+
 const Header = () => {
   return (
     <header id="about" className="header">
@@ -41,9 +102,15 @@ const Header = () => {
               <div key={category} className="skill-category">
                 <h4>{category.charAt(0).toUpperCase() + category.slice(1)}</h4>
                 <div className="tags">
-                  {items.map((item, i) => (
-                    <span key={i}>{item}</span>
-                  ))}
+                  {items.map((item, i) => {
+                    const Icon = iconMap[item];
+                    return (
+                      <span key={i} className="skill-tag">
+                        {Icon && <Icon style={{ width: 20, height: 20, verticalAlign: 'middle', marginRight: 6 }} />}
+                        {item}
+                      </span>
+                    );
+                  })}
                 </div>
               </div>
             ))}
