09db176f484150df60ae39fffef04bac85a52062
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-06-15 23:02:48 -0600

    Update experience

--- src/data/experience.js
+++ src/data/experience.js
@@ -3,22 +3,20 @@ export const experience = [
     title: "Web Software Developer",
     place: "Visualization Design Lab (VDL), University of Utah",
     placeLink: "https://vdl.sci.utah.edu/",
-    duration: "Jan 2024 - Present",
+    duration: "Mar 2025 - Present",
     points: [
-      "Developed and maintained the lab's website using React.js, ensuring optimal performance and user experience",
-      "Implemented responsive design principles to ensure seamless viewing across all devices",
-      "Collaborated with researchers to showcase their work and publications effectively"
+      "Developed a web-based platform for creating, publishing, and sharing interactive data visualization studies."
     ]
   },
   {
     title: "Research Assistant",
     place: "Personal Data and Empowerment Lab (PeDEL), University of Utah",
     placeLink: "https://pedel.cs.utah.edu/",
-    duration: "Aug 2023 - Present",
+    duration: "Aug 2023 - Dec 2024",
     points: [
-      "Conducting research on accessibility and human-computer interaction",
-      "Collaborating with team members on various projects related to user experience and interface design",
-      "Contributing to academic publications and presentations"
+      "Co-authored three Human-Computer Interaction research papers, effectively translating theoretical concepts into practical solutions to enhance user experience.",
+      "Collected and analyzed 1,000+ Reddit posts focused on spinal cord injuries, categorizing how individuals adapt to new physical abilities.",
+      "Managed a self-tracking system for power wheelchairs, ensuring seamless data collection."   
     ]
   }
 ]; 
